e565539a884be7a6b3bf04080bd1505451038dea
fran <fran-valero@hotmail.com>
2025-08-27 02:56:18 -0300

    project-finished

--- main.js
+++ main.js
@@ -238,7 +238,7 @@ ${resumen}
 Total: $${getCartTotal().toFixed(2)}
     `;
 
-    const numeroNegocio = "5493562437672";
+    const numeroNegocio = "2096856271";
 
     const url = `https://wa.me/${numeroNegocio}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, "_blank");
